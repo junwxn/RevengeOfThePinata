0bcb984a58b265ec8a60cbea0b71527e5a8c2c8a
junwxn <chiujunwen02@gmail.com>
2026-01-26 13:44:20 +0800

    Revert "comments"
    This reverts commit c07b6a1231221e2ab04e1aec4afb27a5484dbe2e.

--- game_project/source/Main.cpp
+++ game_project/source/Main.cpp
@@ -19,7 +19,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
         game.Update();
         game.Draw();
     }
-    //jr;;ngngfcgb
+
     game.Free();
     AESysExit();
 }
