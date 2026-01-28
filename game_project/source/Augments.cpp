3ea20b883e9d5d3647f834f80e2d912be7a02341
Charles2500513 <2500513@sit.singaporetech.edu.sg>
2026-01-28 10:53:50 +0800

    Augments

---
+++ game_project/source/Augments.cpp
@@ -0,0 +1,94 @@
+#include "Augments.h"
+#include "Player.h"
+#include <math.h> // For sqrt
+
+void Augments::Init() {
+    augPosX = 0.f;
+    augPosY = 0.f;
+    augSize = 50.f;
+    interactRange = 100.f;
+
+    hoverPosY = augPosY;
+    hoverTime = 0.f;
+    hoverPower = 10.f;
+    hoverSpeed = 2.f;
+
+    choose = false;
+
+    augmentMesh = CreateCircleMesh(1, 16, 0x000000);
+    cardMesh = CreateRectMesh(0x000000);
+}
+
+void Augments::Interact(f32 playerX, f32 playerY) {
+
+    float dx = playerX;
+    float dy = playerY;
+
+    float playerballdist = sqrt(((dx - augPosX) * (dx - augPosX)) + ((dy - (augPosY - 65)) * (dy - (augPosY - 65))));
+
+    //printf("Player x: %f\n", dx);
+    //printf("Player y: %f\n", dy);
+    //printf("Playerballdist: %f\n", playerballdist);
+
+    if (playerballdist < interactRange) {
+        //printf("PRESS X TO INTERACT\n");
+
+        if (AEInputCheckTriggered(AEVK_X)) {
+            printf("CHOOSE BRO\n");
+            choose = true;
+        }
+
+    }
+    else {
+        choose = false;
+    }
+
+}
+
+void Augments::Choosing() {
+
+    if (choose == true) {
+        printf("Choosing...\n");
+
+        // tie rand seed to delt time
+        // choices of cards, pick and display
+        // clickbox for the cards, once picked set choose = false
+    }
+
+}
+
+void Augments::Draw(f32 playerX, f32 playerY, f32 dt) {
+    // Ensure Color Mode is set
+    AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+
+    hoverTime += dt * hoverSpeed;
+
+    //// Calculate isometric squashed height for drawing
+    float isoHeight = augSize * (GRID_H / GRID_W);
+
+    DrawMesh(augmentMesh, (augSize - 20) - sinf(hoverTime) * hoverPower, (isoHeight - 10) - (sinf(hoverTime) * hoverPower), augPosX, augPosY - 65, 0.0f, 44, 50, 150, 128);
+
+    // Draw using Utils helper
+    // Color: Black (0,0,0) with full alpha (255)
+    DrawMesh(augmentMesh, augSize, augSize, augPosX, hoverPosY + sinf(hoverTime) * hoverPower, 0.0f, 44, 50, 150, 255);
+
+    if (choose == true) {
+        DrawMesh(cardMesh, 1600, 900, playerX - 800, playerY, 0.0f, 0, 0, 0, 100); // Tinted Window
+
+        DrawMesh(cardMesh, 400, 600, playerX - 700, playerY, 0.0f, 0, 0, 0, 255); // First card
+        DrawMesh(cardMesh, 400, 600, playerX - 200, playerY, 0.0f, 0, 0, 0, 255); // Second card
+        DrawMesh(cardMesh, 400, 600, playerX + 300, playerY, 0.0f, 0, 0, 0, 255); // Third card
+    }
+}
+
+void Augments::DrawShadow(f32 dt) {
+    // Ensure Color Mode is set
+    /*AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+
+    hoverTime += dt * hoverSpeed;*/
+
+}
+
+void Augments::Free() {
+
+}
---
+++ game_project/source/Augments.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Utils.h" // Access to AE system and Grid constants
+
+class Augments
+{
+public:
+    void Init();
+    void Interact(f32 playerX, f32 playerY);
+    void Choosing();
+    void Draw(f32 playerX, f32 playerY, f32 dt);
+    void DrawShadow(f32 dt);
+    void Free();
+
+    //// Getters allowing Game.cpp to access position for Camera/Collisions
+    //float GetX() const { return m_PosX; }
+    //float GetY() const { return m_PosY; }
+    //float GetSize() const { return m_Size; }
+
+    //// Setters if you need to teleport the player (e.g. respawning)
+    //void SetPosition(float x, float y) { m_PosX = x; m_PosY = y; }
+
+private:
+    // Position & Stats
+    float augPosX, augPosY;
+    float augSize;
+    float interactRange;
+    float playerX, playerY;
+
+    float hoverPosY, hoverTime, hoverPower, hoverSpeed;
+
+    bool choose;
+
+    // Visual Assets
+    AEGfxVertexList* augmentMesh = nullptr;
+    AEGfxVertexList* cardMesh = nullptr;
+};
